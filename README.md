6a6f0f6b0bd922c4e7e95de0f56a2e5fac3f8621
Roman Voitalik <voitalik1@gmail.com>
2022-09-30 23:33:38 +0300

    add prject files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# taxi-service
+# Taxi-service
+Implement a simple taxi service that allows for the management of cars, drivers, and manufacturers.
+
+* implemented authentication and authorization for drivers
+* register new driver
+* add/show cars and related drivers
+* delete car
+* add/show drivers and related cars
+* delete driver
+* add/show manufactures
+* delete manufacturer
+
+The project uses Java Servlet and JSP for processing requests from clients.
+Tomcat Server is used as a servlet container.
+Data storage is relational database MySQL. Access to the database is implemented by JDBC technology.
+
+Instruction to compile and launch the application.
+1. Set up database structure. You can execute SQL script src/main/resources/init_db.sql in any admin panel of your MySQL.
+2. Setup connection attributes to your database into ConnectionUtil class.
+3. Configure Tomcat server: add artifact for deploying.
+4. Run Tomcat configuration.
+
+You can test the application at the next address https://arcane-taiga-63211.herokuapp.com
---
+++ src/main/java/taxi/controller/driver/AddDriverController.java
@@ -0,0 +1,39 @@
+package taxi.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+public class AddDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException, ServletException {
+        String login = req.getParameter("login");
+        if (driverService.findByLogin(login).isPresent()) {
+            req.setAttribute("errorMsg", "User already exists.");
+            req.getRequestDispatcher("/WEB-INF/views/drivers/add.jsp").forward(req, resp);
+            return;
+        }
+        String name = req.getParameter("name");
+        String licenseNumber = req.getParameter("license_number");
+        String password = req.getParameter("password");
+        Driver driver = new Driver(name, licenseNumber, login, password);
+        driverService.create(driver);
+        resp.sendRedirect(req.getContextPath() + "/drivers/add");
+    }
+}
