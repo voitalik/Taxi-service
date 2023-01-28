afc58d8008ed3aee000907bc10e12cac3bf1b4f2
Roman Voitalik <voitalik1@gmail.com>
2023-01-28 23:45:40 +0200

    added a check if the user is logged in and wants to log in again

--- src/main/java/taxi/filter/AuthenticationFilter.java
+++ src/main/java/taxi/filter/AuthenticationFilter.java
@@ -30,6 +30,10 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession();
         Long driverId = (Long) session.getAttribute("driver_id");
+        if (driverId != null && req.getServletPath().equals("/login")) {
+            resp.sendRedirect("/index");
+            return;
+        }
         if (driverId == null && allowedUrls.contains(req.getServletPath())) {
             chain.doFilter(request, response);
             return;
